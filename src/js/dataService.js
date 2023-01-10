dc8c1a3fb65990f8a42ef3864a2272de9937fb2a
vitaliimalynka <vitalii.malynka@gmail.com>
2023-01-10 22:52:39 +0200

    add structure js

---
+++ src/js/dataService.js
@@ -0,0 +1,10 @@
+class DataService {
+    #baseUrl = 'http://api.openweathermap.org/data/2.5/weather';
+    #appId = 'bf35cac91880cb98375230fb443a116f'
+
+    getWeatherForecast(cityId) {
+        
+    }
+}
+
+export const dataService = new DataService()
---
+++ src/js/state.js
@@ -0,0 +1,4 @@
+export const STATE = {
+    citiesList: [703448, 2643743, 5128638], //Kyiv, London, NY
+    currentForecastList: []
+}
